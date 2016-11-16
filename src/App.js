79dd5cefbc7cf7281a8fef1fd97e5cd3252b529e
Franck Ernewein <franck.ernewein@gmail.com>
2016-11-16 11:34:33 +0100

    feat: add static data with a JSON file
      and display posts in a very basic way

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import posts from './data.json';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="wall">
+      {posts.map((post, i) => {
+        return <div key={i} className="post">
+          <h2>@{post.user.name}</h2>
+          <p>{post.text}</p>
+          <em>from {post._source}</em>
+        </div>;
+      })}
+    </div>;
   }
 }
 
