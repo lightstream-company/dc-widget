d70bf196afd32dc5f25a8fd3a7d0c57d945da3e1
Franck Ernewein <franck.ernewein@gmail.com>
2016-11-24 16:26:06 +0100

    task: load historical data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+//import { createStore } from 'redux';
+import createConnection from 'tweetping-connect';
 import './index.css';
 
+const {load} = createConnection('f2e596ea');
+
+load('wall/', (posts) => {
+  console.log(posts);
+});
+
+const store = {};
+
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
