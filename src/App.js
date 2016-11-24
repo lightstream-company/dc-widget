462983dcd95241d507155970e8587ea3c57a3d53
Thomas Tuvignon <thomas.tuvignon@gmail.com>
2016-11-24 14:17:31 +0100

    Ajouts JS

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import posts from './data.json';
 
+console.log(posts);
+
 class App extends Component {
   render() {
     return <div className="wall">
       {posts.map((post, i) => {
         return <div key={i} className="post">
+          <img src={post.user.profile_picture} alt="" />
           <h2>@{post.user.name}</h2>
           <p>{post.text}</p>
           <em>from {post._source}</em>
