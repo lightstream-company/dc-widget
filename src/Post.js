b9e71dda7fc59608fc3dc24fc31711451ea2608c
Franck Ernewein <franck.ernewein@gmail.com>
2016-11-28 11:00:28 +0100

    fix: zIndex for profile picture
      & opacity for fader screen

--- src/Post.js
+++ src/Post.js
@@ -8,10 +8,12 @@ const {permalink} = createConnection(getStreamId());
 export default function Post(post) {
   const {index, user, size, text, _id} = post;
   const {profile_picture} = user;
-  const opacity = size - 1 === index ? 0 : 0.9; //0.5 + (size - index) / size * 0.5;
+  const opacity = size - 1 === index ? 0 : 1; //0.5 + (size - index) / size * 0.5;
   const rotate = index / (size - 1) * -360 - 90;
+  const zIndex = (size + 1) - index;
   const translate = 100;
   const style = {
+    zIndex,
     opacity,
     transform: `rotate(${rotate}deg) translate(${translate}px)`
   };
