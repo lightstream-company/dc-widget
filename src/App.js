8247b01074403a212c1ac0a77e838b182c7f440a
Franck Ernewein <franck.ernewein@gmail.com>
2016-11-29 18:20:27 +0100

    Example to display one picture in the post

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ class App extends Component {
     const {posts} = this.props;
     return <div className="wall">
       {posts.map((post, i) => {
+
+        var thumb;
+        if(post.media && post.media[0] && post.media[0].images){
+          thumb = <div className="media">
+            <img src={post.media[0].images.large.url} alt="" />
+          </div>;
+        }
+
+
         return <div key={i} className="post">
-          <img src={post.user.profile_picture} alt="" />
+          <img src={post.user.profile_picture} alt="" className="profile-picture" />
           <h2>@{post.user.name}</h2>
           <em>from {post._source}</em>
           <p>{post.text}</p>
+          {thumb}
         </div>;
       })}
     </div>;
